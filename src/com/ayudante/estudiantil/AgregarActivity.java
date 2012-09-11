eb336a7e01135d5d7efdd0f20c5599ed4030efae
Juan Guillermo Guzman Clavijo <jggc94@gmail.com>
2012-09-10 20:55:21 -0500

    Primer Commit Ayudante Estudiantil

---
+++ src/com/ayudante/estudiantil/AgregarActivity.java
@@ -0,0 +1,231 @@
+package com.ayudante.estudiantil;
+
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.dataframework.DataFramework;
+import com.android.dataframework.Entity;
+
+public class AgregarActivity extends Activity
+{
+	private static final int DIALOG_FECHA = 0;
+	private int anioP;
+    private int mesP;
+    private int diaP;
+    private StringBuilder fechaString;
+    
+	private EditText fieldTitulo, fieldTexto;
+	private Button btnAgregar,btnFecha;
+	private TableLayout tabla;
+	private TableRow row;
+	private TextView titulo,texto,fecha;
+	private Context contexto;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.agregar_activity);
+		
+		contexto = getApplicationContext();
+		
+		fieldTitulo = (EditText) findViewById(R.id.fieldTitulo);
+		fieldTexto = (EditText) findViewById(R.id.fieldTexto);
+		
+		btnAgregar = (Button) findViewById(R.id.btnAgregarNueva);
+		btnAgregar.setOnClickListener(new OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				if(!fieldTitulo.getText().toString().equals("") && !fieldTexto.getText().toString().equals("") && btnFecha.getText().toString().compareToIgnoreCase("Fecha") != 0)
+				{
+					Entity ent = new Entity("tarea");
+					ent.setValue("TITULO", fieldTitulo.getText().toString());
+					ent.setValue("TEXTO", fieldTexto.getText().toString());
+					ent.setValue("FECHA", fechaString);
+					ent.save();
+					
+					crearHeaderTabla();
+					cargarDatos();
+					Toast.makeText(contexto, "Tarea Agregada!!!", Toast.LENGTH_SHORT).show();
+				}
+				else
+				{
+					Toast.makeText(contexto, "Rellena Los Campos", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+		
+		btnFecha = (Button) findViewById(R.id.btnFecha);
+		btnFecha.setOnClickListener(new OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				showDialog(DIALOG_FECHA);
+			}
+		});
+		
+		Calendar cal = Calendar.getInstance();
+        anioP = cal.get(Calendar.YEAR);
+        mesP = cal.get(Calendar.MONTH);
+        diaP = cal.get(Calendar.DAY_OF_MONTH);
+        
+        updateDisplay();
+        
+		tabla = (TableLayout) findViewById(R.id.tablelayoutVer);
+		crearHeaderTabla();
+		cargarDatos();
+		
+	}
+
+    public void crearHeaderTabla()
+    {
+    	tabla.removeAllViews();
+    	
+      	row = new TableRow(contexto);
+
+        titulo = new TextView(contexto);
+        titulo.setWidth(100);
+        titulo.setText(getString(R.string.hintTitulo));
+        titulo.setTextSize(12);
+        titulo.setTextColor(Color.WHITE);
+        titulo.setBackgroundColor(Color.parseColor("#DD5400"));
+        titulo.setGravity(Gravity.CENTER);
+        
+      	texto = new TextView(contexto);
+      	texto.setWidth(100);
+      	texto.setText(getString(R.string.hintCuerpoTexto));
+      	texto.setTextSize(12);
+      	texto.setTextColor(Color.WHITE);
+      	texto.setBackgroundColor(Color.parseColor("#FF9C00"));
+      	texto.setGravity(Gravity.CENTER);
+        
+      	fecha = new TextView(contexto);
+      	fecha.setWidth(100);
+        fecha.setText("Fecha");
+        fecha.setTextSize(12);
+        fecha.setWidth(100);
+        fecha.setTextColor(Color.WHITE);
+        fecha.setBackgroundColor(Color.parseColor("#DD5400"));
+        fecha.setGravity(Gravity.CENTER);
+        
+        row.addView(titulo);
+        row.addView(texto);
+        row.addView(fecha);
+        tabla.addView(row);
+    }
+    
+    public void cargarDatos()
+    {
+    	 List<Entity> list = DataFramework.getInstance().getEntityList("tarea");
+         @SuppressWarnings("rawtypes")
+         Iterator iter = list.iterator();
+         
+         // Mientras exista uno mas
+         while (iter.hasNext()) 
+         {
+         	Entity ent = (Entity)iter.next();
+         	String tituloP = ent.getString("TITULO");
+         	String textoP = ent.getString("TEXTO");
+         	String fechaP = ent.getString("FECHA");
+         	
+         	rellenarTablaConRows(tituloP, textoP, fechaP);
+         }
+    }
+    
+    public void rellenarTablaConRows(String tituloP, final String textoP, String fechaP)
+    {	
+    	row = new TableRow(contexto);
+     	
+    	titulo = new TextView(contexto);
+    	titulo.setWidth(100);
+        titulo.setText(tituloP);
+        titulo.setTextSize(10);
+        titulo.setTextColor(Color.BLACK);
+        titulo.setGravity(Gravity.CENTER);
+
+        texto = new TextView(contexto);
+        texto.setWidth(100);
+        texto.setText(textoP);
+        texto.setTextSize(10);
+        texto.setWidth(100);
+        texto.setTextColor(Color.BLACK);
+        texto.setGravity(Gravity.CENTER);
+        
+        fecha = new TextView(contexto);
+        fecha.setWidth(100);
+        fecha.setText(fechaP);
+        fecha.setTextSize(10);
+        fecha.setWidth(100);
+        fecha.setTextColor(Color.BLACK);
+        fecha.setGravity(Gravity.CENTER);
+        
+        row.addView(titulo);
+        row.addView(texto);
+        row.addView(fecha);
+ 		row.setOnClickListener(new OnClickListener() 
+ 		{
+			public void onClick(View v) 
+			{
+				Toast.makeText(contexto, "La Tarea es : " + textoP, Toast.LENGTH_SHORT).show();
+			}
+		});
+ 		
+        tabla.addView(row);
+    }
+    
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() 
+    {
+    	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) 
+    	{
+    		anioP = year;
+    		mesP = monthOfYear;
+            diaP = dayOfMonth;
+            updateDisplay();
+            displayToast();
+    	}
+	};
+	
+	private void updateDisplay() 
+	{
+		fechaString = (new StringBuilder().append(mesP + 1).append("/").append(diaP).append("/").append(anioP).append(" "));
+		btnFecha.setText(fechaString);
+	}
+	
+	private void displayToast() 
+	{
+        Toast.makeText(this, new StringBuilder().append("Date choosen is ").append(fechaString),  Toast.LENGTH_SHORT).show();
+    }
+	
+	protected Dialog onCreateDialog(int id) 
+	{
+        switch (id) {
+        case DIALOG_FECHA:
+            return new DatePickerDialog(this,dateSetListener,anioP, mesP, diaP);
+        }
+        return null;
+    }	
+}
---
+++ src/com/ayudante/estudiantil/MainActivity.java
@@ -0,0 +1,125 @@
+package com.ayudante.estudiantil;
+
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.android.dataframework.DataFramework;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private ImageButton btnAgregar;
+	private ProgressDialog dialog;
+	private Context contexto;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    
+        contexto = getApplicationContext();
+        
+        btnAgregar = (ImageButton) findViewById(R.id.btnAgregar);
+        
+        btnAgregar.setOnClickListener(new OnClickListener() 
+        {
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getApplicationContext(), AgregarActivity.class);
+				startActivity(intent);
+			}
+		});
+        
+        try {
+			DataFramework.getInstance().open(contexto, getPackageName());
+		} catch (XmlPullParserException e) 
+		{
+			Toast.makeText(getApplicationContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+		} catch (IOException e) 
+		{
+			Toast.makeText(getApplicationContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+		}
+//        dialog = new ProgressDialog(contexto);
+//		dialog.setMessage(getString(R.string.cargando));
+//		dialog.setCancelable(false);
+//		new ProgressAbriendo().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item) 
+	{	
+    	if(item.getItemId() == R.id.exit)
+		{
+			finish();
+		}
+		return true;
+	}
+	
+    public class ProgressAbriendo extends AsyncTask<Void, Integer, Void>
+	{
+		int progress;
+
+		/**
+		 * Metodo para despues que pasa el Thread
+		 */
+		@Override
+		protected void onPostExecute(Void result) 
+		{
+			// Cierra el dialog
+			dialog.dismiss();
+		}
+
+		/**
+		 * Metodo para antes de empezar el Thread
+		 */
+		@Override
+		protected void onPreExecute() 
+		{
+			// Muestra el dialog
+			dialog.show();
+			progress = 0;
+		}
+		@Override
+		protected void onProgressUpdate(Integer... values) 
+		{
+			dialog.setProgress(values[0]);
+		}
+
+		/**
+		 * Metodo usado mientras se hace el Thread
+		 */
+		@Override
+		protected Void doInBackground(Void... params) 
+		{
+			try 
+			{
+				// Abre la base de datos
+				DataFramework.getInstance().open(contexto, getPackageName());
+	    	} catch (Exception  e) 
+			{
+				dialog.dismiss();
+				Toast.makeText(getApplicationContext(), getString(R.string.errorAbriendoBD), Toast.LENGTH_SHORT).show();
+			}
+			return null;
+		}
+		
+	}
+}
